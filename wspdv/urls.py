1f243d935ce794a0653d287f13bf63aead99c2f9
Walter Pereira Da Silva <waltersilva5@outlook.com>
2021-05-03 22:33:22 -0300

    menus finalizados

--- wspdv/urls.py
+++ wspdv/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 #views
-from wspdv.views import index
+from wspdv.views import index, vendas, produtos, clientes, caixa
 urlpatterns = [
    path('', index, name='index'),
+   path('vendas', vendas, name='vendas'),
+   path('produtos', produtos, name='produtos'),
+   path('clientes', clientes, name='clientes'),
+   path('caixa', caixa, name='caixa'),
 ]
--- wspdv/views.py
+++ wspdv/views.py
@@ -4,3 +4,17 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request, 'index.html')
+
+def vendas(request):
+    return render(request, 'vendas.html')
+
+def produtos(request):
+    return render(request, 'produtos.html')
+
+def clientes(request):
+    return render(request, 'clientes.html')
+
+def caixa(request):
+    return render(request, 'caixa.html')
+
+
