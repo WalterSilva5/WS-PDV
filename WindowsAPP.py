5780069b74e172fa80301925cb3c4abfd366a37f
Walter Pereira Da Silva <waltersilva5@outlook.com>
2021-05-06 20:20:37 -0300

    working 2

--- WindowsAPP.py
+++ WindowsAPP.py
@@ -1,18 +1,24 @@
 import sys
+from PyQt5.QtCore import QUrl
+from PyQt5.QtWebEngineWidgets import QWebEngineView
+from PyQt5.QtWidgets import QApplication
 
-from PyQt5.QtCore import *
-from PyQt5.QtGui import *
-from PyQt5.QtWidgets import *
-from PyQt5.QtWebKit import *
-from PyQt5.QtWebKitWidgets import *
-from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow
+
+def callback_function(html):
+    arq=open("cupom.txt", 'w')
+    arq.write(str(html))
+    arq.close()
+
+
+
+def on_load_finished():
+    web.page().runJavaScript("document.documentElement.outerHTML", callback_function)
+
 
 app = QApplication(sys.argv)
-
-web = QWebView()
+web = QWebEngineView()
 web.load(QUrl("http://localhost"))
-web.setWindowTitle("WALTERSILVA5 PDV")
-web.setGeometry(100, 100, 600, 400)
-web.showMaximized()
+web.show()
+#web.loadFinished.connect(on_load_finished)
 
 sys.exit(app.exec_())
